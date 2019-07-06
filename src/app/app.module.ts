54b0bd62bbde4102b7e0afe447bbac43cfa973b4
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-06 12:18:24 +0200

    app html and angular/cdk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VirtualComponent } from './virtual/virtual.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VirtualComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/virtual/virtual.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-virtual',
+  templateUrl: './virtual.component.html',
+  styles: []
+})
+export class VirtualComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
